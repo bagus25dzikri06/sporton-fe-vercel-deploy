3dfb29584b8b13b811f1fa52ad95ed7138a4e0b1
bagus25dzikri06 <bagusdzikri@gmail.com>
2026-01-11 22:47:05 +0700

    Update title in Payment page

--- app/(landing)/payment/page.tsx
+++ app/(landing)/payment/page.tsx
@@ -4,7 +4,7 @@ import PaymentSteps from "../components/payment/payment-steps"
 const Payment = () => {
     return <main className="bg-gray-100 min-h-[80vh]">
         <div className="max-w-5xl mx-auto pt-15 pb-10">
-            <h1 className="text-5xl font-bold text-center mb-11">Checkout Now</h1>
+            <h1 className="text-5xl font-bold text-center mb-11">Payment</h1>
         </div>
         <div className="grid grid-cols-2 gap-14 pb-20 px-25">
             <PaymentOptions />
