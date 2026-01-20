feed7aedee925efddb441e9f0278ecd71936341b
bagus25dzikri06 <bagusdzikri@gmail.com>
2026-01-20 21:02:59 +0700

    Update in changing the data type of SportOn's customer contact

--- app/hooks/use-cart-store.ts
+++ app/hooks/use-cart-store.ts
@@ -8,7 +8,7 @@ export interface CartItem extends Product {
 
 export interface CustomerInfo {
     customerName : string;
-    customerContact: string | null;
+    customerContact: string;
     customerAddress : string;
 }
 
